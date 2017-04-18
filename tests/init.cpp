ac013ac42eb2f3bbdd1c68e4a929ff69e8f92c3c
SVolkoff <sabina-volkoff@yandex.com>
2017-04-18 13:49:50 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,28 @@
 #include <vector.hpp>
 #include <catch.hpp>
 
-SCENARIO("vector init", "[init]") {
+SCENARIO(" init")
+{
 	vector_t vector;
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
 }
+
+SCENARIO(" init with param")
+{ 
+	vector_t vector(3); 
+	REQUIRE(vector.size() == 3); 
+	REQUIRE(vector.capacity() == 3); 
+} 
+SCENARIO("vector init with object")
+{ 
+	vector_t vector; 
+	vector.push_back(5); 
+	vector.push_back(6); 
+	vector_t vector2(vector);
+	REQUIRE(vector.size() == vector2.size());
+	REQUIRE(vector.capacity() == vector2.capacity());
+	REQUIRE(vector[0] == vector2[0]);
+	REQUIRE(vector[1] == vector2[1]);
+}
+
