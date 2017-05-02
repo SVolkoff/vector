43b003677af2ba950d9b04906112a7e0597cff6f
SVolkoff <sabina-volkoff@yandex.com>
2017-05-02 14:18:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -31,10 +31,10 @@ SCENARIO("=")
 	vector.push_back(1); 
 	vector.push_back(2);  
 	REQUIRE(vector.size() == 2); 
-	REQUIRE(vector.capacity() == 6); 
+	REQUIRE(vector.capacity() == 2); 
 	vector2 = vector; 
 	REQUIRE(vector2.size() == 2); 
-	REQUIRE(vector2.capacity() == 6);
+	REQUIRE(vector2.capacity() == 2);
 	REQUIRE(vector2[0]==1);
 	REQUIRE(vector2[1]==2);
 } 
@@ -45,7 +45,7 @@ SCENARIO("push_back")
 	vector.push_back(2);
 	vector.push_back(3);
 	REQUIRE(vector.size() == 5);
-	REQUIRE(vector.capacity() == 7);
+	REQUIRE(vector.capacity() == 8);
 }
 SCENARIO("[]")
 {
@@ -75,7 +75,7 @@ SCENARIO("delete")
 	REQUIRE(vector.capacity() == 4);
 	REQUIRE(vector.delval()==1);
 	REQUIRE(vector.size() == 2);
-	REQUIRE(vector.capacity() == 2);
+	REQUIRE(vector.capacity() == 4);
 	REQUIRE(vector.delval()==1);
 	REQUIRE(vector.size() == 1);
 	REQUIRE(vector.capacity() == 2);
